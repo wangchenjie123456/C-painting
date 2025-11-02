2fd2dce2c117d2ac3c12e4a3f8cf3faae1352f47
wangchenjie123456 <3313568374@qq.com>
2025-11-02 15:14:13 +0800

    Add files via upload

---
+++ C#画图/luxintao/sethatch_form.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+/**
+ * 纹理设置
+ * */
+namespace luxintao
+{
+    public partial class sethatch_form : Form
+    {
+        private form1 mainForm;
+        private HatchStyle[] hs;
+        private HatchBrush hb;
+        public sethatch_form()
+        {
+            InitializeComponent();
+            hs = new HatchStyle[] { HatchStyle.BackwardDiagonal, HatchStyle.Cross, HatchStyle.DarkDownwardDiagonal,
+                HatchStyle.DarkHorizontal, HatchStyle.DarkUpwardDiagonal, HatchStyle.DarkVertical,
+                HatchStyle.DashedDownwardDiagonal, HatchStyle.DashedHorizontal, HatchStyle.DashedUpwardDiagonal,
+                HatchStyle.DashedVertical, HatchStyle.DiagonalBrick, HatchStyle.DiagonalCross, HatchStyle.Divot, 
+                HatchStyle.DottedDiamond, HatchStyle.DottedGrid, HatchStyle.ForwardDiagonal, HatchStyle.Horizontal,
+                HatchStyle.HorizontalBrick, HatchStyle.LargeCheckerBoard, HatchStyle.Vertical, HatchStyle.Wave,
+                HatchStyle.ZigZag };
+        }
+
+        private void LXT_sethatch_form_Load(object sender, EventArgs e)
+        {
+            mainForm = (form1)this.Owner;
+            frontColorBT.BackColor = mainForm.StartColor;//前景色，需要对主界面修改
+            backColorBT.BackColor = mainForm.EndColor;//背景色，需要对主界面修改
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            //为纹理类型下拉列表赋值，并确定选中项
+            for (int i = 0; i < hs.Length;i++)
+            {
+                comboBox1.Items.Add(hs[i].ToString());
+                if (mainForm.HS.ToString() == hs[i].ToString()) {
+                    comboBox1.SelectedIndex = i;
+                }
+            }
+        }
+
+        
+
+
+        private void preview() {
+            Graphics g = CreateGraphics();
+            Rectangle rec = new Rectangle(label4.Left+50,label4.Top-20,163,51);
+            hb = new HatchBrush(hs[comboBox1.SelectedIndex],frontColorBT.BackColor,backColorBT.BackColor);
+            g.FillEllipse(hb,rec);
+        }
+
+        /**
+         *paint事件
+         **/
+        private void LXT_sethatch_form_Paint(object sender, PaintEventArgs e)
+        {
+            preview();
+        }
+
+        /**
+         * 
+         * 前景色点击
+         * **/
+        private void frontColorBT_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.AllowFullOpen = true;
+            dialog.Color = frontColorBT.BackColor;
+            if(dialog.ShowDialog()==DialogResult.OK){
+                frontColorBT.BackColor = dialog.Color;
+                preview();
+            }
+        }
+        /**
+       
+         * 背景色
+         * **/
+        private void backColorBT_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.AllowFullOpen = true;
+            dialog.Color = backColorBT.BackColor;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                backColorBT.BackColor = dialog.Color;
+                preview();
+            }
+        }
+
+
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            preview();
+        }
+
+        
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            mainForm.StartColor = frontColorBT.BackColor;
+            mainForm.EndColor = backColorBT.BackColor;
+            mainForm.HS = hs[comboBox1.SelectedIndex];
+            mainForm.HB = hb;
+            mainForm.ColorType = 3;
+            this.Close();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ C#画图/luxintao/setsize_form.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace luxintao
+{
+    public partial class setsize_form : Form
+    {
+        private form1 sizeForm;
+
+        public setsize_form()
+        {
+            InitializeComponent();
+
+        }
+        /**
+         * 取消按钮点击事件
+         * **/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lxt_setsize_form_Load(object sender, EventArgs e)
+        {
+            sizeForm =(form1)this.Owner;
+            currWidthLabel.Text = sizeForm.ImageWidth.ToString();
+            currHeightLabel.Text = sizeForm.ImageHeight.ToString();
+        }
+
+        /**
+         * 确认按钮点击事件
+         * */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(myWidthTB.Text.Equals("")||myHeightTB.Text.Equals("")){
+                MessageBox.Show("高度与宽度不能为空", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            sizeForm.ImageWidth = int.Parse(myWidthTB.Text.ToString());
+            sizeForm.ImageHeight = int.Parse(myHeightTB.Text.ToString());
+            this.Close();
+        }
+
+
+
+
+    }
+}
